f61920376b3d89f852bc4e846e384c13541d8171
thales <thales.rieger@aluno.cps.sp.gov.br>
2026-06-23 14:43:50 -0300

    feito

---
+++ 777.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int saldo = 1000.00;
+    int emprestimo = 0.0;
+    int opcao;
+    int valor;
+
+    while (1) {
+    printf("\n CAIXA ELETRONICO \n");
+    printf("1. Verificar Saldo\n");
+    printf("2. Depositar Valor\n");
+    printf("3. Sacar Valor\n");
+    printf("4. Pedir Emprestimo\n");
+    printf("5. Pagar Emprestimo\n");
+    printf("6. Consultar Emprestimo\n");
+    printf("7. Sair\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+   
+   }}
