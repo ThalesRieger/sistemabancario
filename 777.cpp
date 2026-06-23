e635393df8dff084baa8bffc0312f0011593b310
thales <thales.rieger@aluno.cps.sp.gov.br>
2026-06-23 15:26:22 -0300

    casos

--- 777.cpp
+++ 777.cpp
@@ -18,4 +18,46 @@ int main() {
     printf("Escolha uma opcao: ");
     scanf("%d", &opcao);
    
-   }}
+    if (opcao == 7) {
+            printf("Saindo do sistema.\n");
+            break;
+    }
+
+        switch (opcao) {
+            	case 1:
+                printf("\nSeu saldo atual e: R$ %.2d\n", saldo);
+                break;
+            	case 2:
+                printf("Digite o valor para deposito: R$ ");
+                scanf("%d", &valor);
+                if (valor > 0) {
+                saldo += valor;
+                printf("Deposito realizado! Novo saldo: R$ %.2d\n", saldo);
+            } else {
+                printf("Valor invalido.\n");
+			}
+				break;
+				case 3:
+					printf("coloque o valor para o saque: R$ ");
+					scanf("%d", &valor);
+					if (valor > 0){
+						saldo -=valor;
+					printf("Valor sacado, saldo atual: R$ %2.d\n", saldo);
+				   } else {
+					printf("valor invalido ou saldo insuficiente\n");
+				}
+				case 4:
+				    if (emprestimo > 0) {
+                    printf("Voce ja possui um emprestimo pendente.\n");
+                } else {
+                    printf("Digite o valor do emprestimo: R$ ");
+                    scanf("%d", &valor);
+                    if (valor > 0) {
+                    emprestimo = valor;
+                    saldo += valor;
+                    printf("Emprestimo de R$ %.2d aprovado. Novo saldo: R$ %.2d\n", valor, saldo);
+                    }}
+                    break;
+            }
+        }
+    }
